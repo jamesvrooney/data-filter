9d31c52c30cd4c0e6cf7732945c6a33caee90bd3
jamesvrooney <jamesvrooney@hotmail.com>
2017-04-13 00:58:00 +0100

    Added extra text skeletons.

--- src/test/java/com/sedex/test/DataFiltererTest.java
+++ src/test/java/com/sedex/test/DataFiltererTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
 
 import static org.junit.Assert.assertTrue;
 
@@ -14,6 +15,30 @@ public class DataFiltererTest {
         assertTrue(DataFilterer.filterByCountry(openFile("src/test/resources/empty"), "GB").isEmpty());
     }
 
+    @Test
+    public void testFilterByCountry() throws FileNotFoundException {
+        assertTrue(!DataFilterer.filterByCountry(openFile("src/test/resources/multi-lines"), "GB").isEmpty());
+    }
+
+    @Test
+    public void testFilterByCountryWithResponseTimeAboveLimit() throws FileNotFoundException {
+        // given
+        String filename = "src/test/resources/multi-lines";
+        Reader source = openFile(filename);
+        String country = "GB";
+        int limit = 56;
+
+        assertTrue(!DataFilterer.filterByCountryWithResponseTimeAboveLimit(source, country, limit).isEmpty());
+    }
+
+    @Test
+    public void testFilterByCountryWithResponseTimeAboveAverage() throws FileNotFoundException {
+        // given
+        String filename = "src/test/resources/multi-lines";
+        Reader source = openFile(filename);
+
+        assertTrue(!DataFilterer.filterByResponseTimeAboveAverage(source).isEmpty());
+    }
 
     private FileReader openFile(String filename) throws FileNotFoundException {
         return new FileReader(new File(filename));
